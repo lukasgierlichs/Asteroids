5b00d3fed53e848afb58edb0aeb87f3bc797c301
lukasgierlichs <lukas.gierlichs@gmail.com>
2025-12-18 19:34:00 +0000

    B: Game so far

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 import pygame
+import sys
 from constants import *
 from logger import log_state
+from logger import log_event
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    shots = pygame.sprite.Group()    
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0        
 
 if __name__ == "__main__":
     main()
---
+++ shot.py
@@ -0,0 +1,23 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS, LINE_WIDTH
+
+Shot.containers = (shots, updatable, drawable)
+
+# Shot class representing a projectile fired by the player
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen,
+            "white",
+            self.position,
+            self.radius,
+            LINE_WIDTH
+        )
+
+    def update(self, dt):
+        print("Updating shot position", self.position)
+        self.position += self.velocity * dt
